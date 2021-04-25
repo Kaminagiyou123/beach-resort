bb6a41d8f5caff7f3d08603ea10158d93bd93f43
Ran You <ran.you8311@gmail.com>
2021-04-24 19:59:03 -0700

    day 1 complete

---
+++ src/styles.js
@@ -0,0 +1,40 @@
+export const setColor = {
+  primaryColor: "#af9a7d",
+  mainWhite: "#fff",
+  mainBlack: "#222",
+  mainGrey: "#ececec",
+  lightGrey: "#f7f7f7",
+};
+
+export const setFont = {
+  main: "font-family: 'Lato', sans-serif;",
+  slanted: "font-family: 'Handlee', cursive;",
+};
+
+export const setFlex = ({ x = "center", y = "center" } = {}) => {
+  return `display:flex;align-items:${y};
+ justify-content:${x}`;
+};
+
+export const setBackground = ({
+  img = `https://images.pexels.com/photos/271639/pexels-photo-271639.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`,
+  color = "rgba(0,0,0,0)",
+} = {}) => {
+  return ` background: linear-gradient(${color},${color}),
+    url(${img}) center/cover fixed no-repeat;`;
+};
+
+export const setRem = (number = 16) => {
+  return `${number / 16}rem`;
+};
+
+export const setLetterSpacing = (number = 2) => {
+  return `letter-spacing:${number}px`;
+};
+export const setBorder = ({
+  width = "2px",
+  style = "solid",
+  color = "black",
+} = {}) => {
+  return `border:${width} ${style} ${color}`;
+};
